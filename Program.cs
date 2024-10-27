a05671bf14da51f42dd99937b2ba508efd4e0bc2
Emzzy241 <emzzyoluwole@gmail.com>
2024-10-27 06:22:59 +0100

    update code file

--- Program.cs
+++ Program.cs
@@ -1,105 +1,85 @@
 using System;
 using System.Globalization; // For importing the CultureInfo object type which returns type System.Globalization.Calendar
+using System.Security.Cryptography;
 
 namespace Learning;
 public class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hey there, lets learn about date's ;)");
+        Console.WriteLine("Hey there, lets learn Cryptography ;)");
 
-        /*
-        var hebrewCalendar = new System.Globalization.HebrewCalendar();
-
-        var theDate = new DateOnly(5785, 3, 3, hebrewCalendar); // first value must be between 5343-5999.. 5785 is equivalent to year 2024
-
-        // The System.Globalization.HebrewCalendar.ToDateTime(Int32 year, Int32 month, Int32 day, Int32 hour, Int32 minute, Int 32 seconds) takes in 
-
-        Console.WriteLine(theDate); // MM/DD/YY
-        */
-
-        // Learning about the DateOnly type
-        var today = DateOnly.FromDateTime(DateTime.Now);
-
-
-        Console.WriteLine($"Today is {today}"); // using the DateOnly.FromDateTime() to Print out today's date
-
-        Console.WriteLine();
-        Console.WriteLine("---------------  LEARNING SOMETHING NEW  -------------------");
-        Console.WriteLine();
-
-        // Methods to adjust a DateOnly structure
-        /*
-            There are three methods used to adjust a DateOnly structure: AddDays(), AddMonths(), AddYears()
-            Each method takes an integer parameter, and increases the date by that measurement.
-            If a negative number is provided, the date is decreased by that measurement. 
-            The methods return a new instance of DateOnly, as the structure is immutable
-        */
-
-        var theDate = new DateOnly(2024, 10, 18); //Yy/Mm/Dd --- HOW TO CREATE A new DateOnly object
-        // or call on DateOnly.FromDateTime(DateTime.Now) to generate the actual date
-
-        var todaysDate = DateOnly.FromDateTime(DateTime.Now);
-        var theNextDay = todaysDate.AddDays(1);
-        var previousDayDate = todaysDate.AddDays(-1);
-        var decadeLater = todaysDate.AddYears(10);
-        var lastMonth = todaysDate.AddMonths(-1);
-
-        var dateInWords = todaysDate.ToLongDateString(); 
-
+        /* Today, I will be learning 7 cryptography concepts from Fireship on Youtube
+        // Cryptography is the one thing that makes the internet secure, without the mysterious art of cryptography, there would be no secrets and no privacy in the digital world
+        // Your emails, bank records and privacy will be easily hackable
+        // Computers do exactly what you tell them to do, but sometimes we don't want them to do some certain things like expose private data
         
+        // Cryptography can take some useful bytes of data, then scramble them up with an algorithm making it nearly impossible for a computer to understand its true meaning without the proper credentials
+
+        // Lets learn 7 useful cryptography concepts that every developers should know
+        // 1. HASH 2. SALT 3. HMAC 4. SYMMETRIC ENCRYPTION 5. KEYPAIRS 6. SIGNING 7. ASSYMETRIC ENCRYPTION
+
+        // Cryptography is the science of creating secrets, it has been around long before the computer, since 1900 BC
+        // Crytography is awlasys evolving, what is considered safe and secret today may not be the case tomorrow.
+        // As a devloper you don't need to understand all the complex math that goes into cryptography but it is essential to understand how certain key concepts work like Hashing, Encryption, and Signing etc.
+
+        */
+
+        // 1. HASH: The hash word means to chop and mix.... Thats what an hasing function does
+        // You start with an input(it can be any length), then pass it off to a hashing function. This function returns a fixed length value of what looks like just meaningless gabbage
+        // The important thing here is that he function would always produce the same output given the same input, it needs to be fast to compute and the output should be unique
+        // However it is very difficult for a computer to reverse engineer what the initial message was.
+        // Hash is really useful because it allows developers to store data without needing to know its true value, 
+        // A simple example is not to storing a password in a database but a PasswordHash, this is becase if an hacker gets a hold of the database, they can put those passwords to use
+        // But if the data is hashed, then they have to also crash that hash to figure out what the true password is:
+        // I already implemented the HASH in Javascript
+        // Hash isn't rally sufficient for storing a password in a database
+
+        // 2. SALT: The fact that a hashing function always returns the same value is a very big problem when it comes to passwords
+        // Users can use can come up with passwords as simple as: password123... If a hacker obtains the database and the passwords are hashed
+        // They can just go to something like a rainbow table that has a bunch of precomputed hashes and find a bunch of commonly used passwords
+        // A Salt is just a random value that is added to the password before it is hashed, therfore making it much harder to get
+
+        // A common topic you might find is HMAC
+        // 3. HMAC: Hash-Based Message Authentication Code, this is a hash that also requires a password
+        // So the only person that can create the same hash signature must also have the corresponding passwords or key.
+        // An example of HMAC is J-W-T(Json web token) for authentication on the web.
+        // When a user logs in on a trusted server it generates a token with its key, then the client and server can pass it back and forth.. The client can be google chrome and the server can be nodejs or kestrel for .net
+        // The server can trust it because it knows that only someone with a trusted key could have generated that hash signature.
+
+    
+        // The above is pretty cool, but what happens when you want to share a secret with someone but you also want to allow them to read the original message
+        // Thats where Encryption comes in
+        // With encryption, we take a message, scramble up the bytes to make it unreadable that is called the cipher text, then provide a key or password allowing somebody else to decrypt it.
+        // cipher text is randomized so that each time you encrypt, you get an entirely different output even if the key and message are the same
+        // There are many times of encryption...
+        // 4. Symmetric encryption: Symmetric means that there is a shared password that both the sender and receiver of the message will need
+        // To implement it in node, checkout symmetric-encrypt.js
+    
+        
+        //5. KeyPair
+        // A big limitation in symmetric encryption is the sharing of keys i.e both sender and receiver of message needs to share a password
+        // And that is not very practical for 2 different parties to agree upon a shared password.. once again math comes to the rescue this time 
+        // in the form of a public-key cryptosystem, instead of one key it uses 2 keys that are mathematically linked, a public key and a private key
+        // private key is kept secret while public key can be shared with other perople
+        // An analogy is a mailbox, anybody can drop you a mail inside a slot(a public key) but to get the mail out you need a private key which can only be dones by the owner of the key
 
 
+        // 6. Asymmetric Encryption
+        // You use Asymmetric encryption anytime you go to a website using https
+        // The browser will automatically find a public key of an SSL certificate installed on the website
+        // That public key is used to encrypt any data that you send to the website, that prevents hackers from gaining anything useful from it in transit
+        // Your data is then decrypted using a private key by the trusted website
+        // Check the asymmetric-encrypt.js file on how to implement it in node 
 
-        // Printing them out 
-        Console.WriteLine($"Today's date is: {todaysDate}");
-        Console.WriteLine();
-        Console.WriteLine($"Today's date in full: {dateInWords}");
-        Console.WriteLine();
-        Console.WriteLine($"Previous day date is: {previousDayDate}");
-        Console.WriteLine();
-        Console.WriteLine($"A decade later from today is: {decadeLater}");
-        Console.WriteLine();
-        Console.WriteLine($"Last month from today was: {lastMonth}");
-        Console.WriteLine();
-
-        Console.WriteLine();
-        Console.WriteLine("---------------  LEARNING SOMETHING NEW  -------------------");
-        Console.WriteLine();
-
-        var newDate = DateOnly.ParseExact("21 Oct 2015", "dd MMM yyyy", CultureInfo.InvariantCulture);
-            // Custom format
-        var newDate2 = DateOnly.Parse("October 21, 2015", CultureInfo.InvariantCulture);
-        // DateOnly
-        // can be compared with other instances. For example, you can check if a date isbefore or after another, or if a date today matches a specific date.
-        Console.WriteLine(newDate.ToString("m", CultureInfo.InvariantCulture));
-        // Month day pattern
-        Console.WriteLine(newDate2.ToString("o", CultureInfo.InvariantCulture));
-        // ISO 8601 format
-        Console.WriteLine(newDate2.ToLongDateString());
-
-        // All calendars in .NET derive from the System.Globalization.Calendar class, which provides 
-        // the base calendar implementation. One of the classes that inherits from the Calendar class is the EastAsianLunisolarCalendar class, which is the base class for all lunisolarcalendars.
-
-        Console.WriteLine();
-        Console.WriteLine("---------------  LEARNING SOMETHING NEW  -------------------");
-        Console.WriteLine();
-
-        var comDate = DateOnly.ParseExact("21 Oct 2015", "dd MMM yyyy", CultureInfo.InvariantCulture); // Custom format
-        var comDate2 = DateOnly.Parse("October 21, 2015", CultureInfo.InvariantCulture);
-
-        var dateLater = comDate.AddMonths(6); // add 6 months to date
-
-        var dateBefore = comDate.AddDays(-10); // remove 10 days from date
-
-        Console.WriteLine($"Consider {comDate} ..."); // considering the custom date
-
-        Console.WriteLine($" Is ' {nameof(comDate2)}' equal? {comDate == comDate2}"); // using either Parse() or ParseExact() should yield the same RESULT
-        Console.WriteLine($" Is {dateLater} after? {dateLater > comDate}");
-        Console.WriteLine($" Is {dateLater} before? {dateLater < comDate}");
-
-        Console.WriteLine($" Is {dateBefore } after? {dateBefore > comDate} ");
-        Console.WriteLine($" Is {dateBefore } before? {dateBefore < comDate} ");
+        // 7. Signing
+        // Encryption is fun but in many cases you don't have to encrypt data but rather validate that the data came from a trusted party
+        // That's where signing comes in... A digital signature analogy; Imagine you are expecting a letter in the mail with some sensitive information
+        // You need to be able to trust that that letter came from the right person. So you require them to sign it in blood, it also can't be tampered with so they put a special seal on it that if broken it indicates that its been tampered with
+        // Digital signatures work this way
+        // The sender of message will use their private key to sign a hash of the original message, the private key guarantees authenticity like blood and the hash guarantees
+        // that the message can't be tampered with because it will produce an entirely different signature
+        // The recipient can can then use the public key to validate the authenticity of the message 
 
     }    
 }
